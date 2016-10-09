5af8ebf4f6b63c5b171c326e118a0d8fd5dab9e3
dupgit <olivier.delhomme@free.fr>
2016-10-09 18:51:48 +0200

    First source file.

---
+++ src/cut.c
@@ -0,0 +1,38 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ *    cut.c
+ *    This file is part of content-define-cut (cdc) project.
+ *
+ *    (C) Copyright 2016 Olivier Delhomme
+ *     e-mail : olivier.delhomme@free.fr
+ *
+ *    "Sauvegarde" is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    "Sauvegarde" is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with "Sauvegarde".  If not, see <http://www.gnu.org/licenses/>
+ */
+
+/**
+ * @file cut.c
+ */
+
+#include "cut.h"
+
+/**
+ * Main function
+ * @param argc : number of arguments given on the command line.
+ * @param argv : an array of strings that contains command line arguments.
+ * @returns always 0
+ */
+int main(int argc, char **argv)
+{
+
+}
